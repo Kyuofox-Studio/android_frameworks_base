5cf7128b862455b0be8af8e2f7e3c8fe96fc240c
JimmyChenSavitech <JimmyChenSavitech@users.noreply.github.com>
2023-06-14 22:19:21 +0800

    Add LHDC support

--- media/java/android/media/AudioSystem.java
+++ media/java/android/media/AudioSystem.java
@@ -243,6 +243,12 @@ public class AudioSystem
     public static final int AUDIO_FORMAT_LDAC           = 0x23000000;
     /** @hide */
     public static final int AUDIO_FORMAT_LC3            = 0x2B000000;
+    // Savitech Patch - START  Offload
+    /** @hide */
+    public static final int AUDIO_FORMAT_LHDC           = 0x28000000;
+    /** @hide */
+    public static final int AUDIO_FORMAT_LHDC_LL        = 0x29000000;
+    // Savitech Patch - END
 
 
     /** @hide */
@@ -254,7 +260,12 @@ public class AudioSystem
             AUDIO_FORMAT_APTX,
             AUDIO_FORMAT_APTX_HD,
             AUDIO_FORMAT_LDAC,
-            AUDIO_FORMAT_LC3}
+            AUDIO_FORMAT_LC3,
+            // Savitech Patch - START  Offload
+            AUDIO_FORMAT_LHDC,
+            AUDIO_FORMAT_LHDC_LL,
+            // Savitech Patch - END
+            }
     )
     @Retention(RetentionPolicy.SOURCE)
     public @interface AudioFormatNativeEnumForBtCodec {}
@@ -287,6 +298,10 @@ public class AudioSystem
             case AUDIO_FORMAT_APTX_HD: return BluetoothCodecConfig.SOURCE_CODEC_TYPE_APTX_HD;
             case AUDIO_FORMAT_LDAC: return BluetoothCodecConfig.SOURCE_CODEC_TYPE_LDAC;
             case AUDIO_FORMAT_LC3: return BluetoothCodecConfig.SOURCE_CODEC_TYPE_LC3;
+            // Savitech Patch - START  Offload
+            case AUDIO_FORMAT_LHDC: return BluetoothCodecConfig.SOURCE_CODEC_TYPE_LHDCV5;
+            case AUDIO_FORMAT_LHDC_LL: return BluetoothCodecConfig.SOURCE_CODEC_TYPE_LHDCV5;
+            // Savitech Patch - END
             default:
                 Log.e(TAG, "Unknown audio format 0x" + Integer.toHexString(audioFormat)
                         + " for conversion to BT codec");
@@ -329,6 +344,14 @@ public class AudioSystem
                 return AudioSystem.AUDIO_FORMAT_LDAC;
             case BluetoothCodecConfig.SOURCE_CODEC_TYPE_LC3:
                 return AudioSystem.AUDIO_FORMAT_LC3;
+            // Savitech Patch - START  Offload
+            case BluetoothCodecConfig.SOURCE_CODEC_TYPE_LHDCV3:
+                return AudioSystem.AUDIO_FORMAT_LHDC;
+            case BluetoothCodecConfig.SOURCE_CODEC_TYPE_LHDCV2:
+                return AudioSystem.AUDIO_FORMAT_LHDC;
+            case BluetoothCodecConfig.SOURCE_CODEC_TYPE_LHDCV5:
+                return AudioSystem.AUDIO_FORMAT_LHDC;
+            // Savitech Patch - END
             default:
                 Log.e(TAG, "Unknown BT codec 0x" + Integer.toHexString(btCodec)
                         + " for conversion to audio format");
